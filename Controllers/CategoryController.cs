1068f6fd7a1e55314ef694dac0fa6b66004bd54e
Hameed Hassan <57669085+33185@users.noreply.github.com>
2022-05-29 21:26:45 +0200

    Add files via upload

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,102 @@
+﻿using AdminDashboard.Models;
+using AdminDashboard.Models.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IProductRepository<Category> categoryRepository;
+        public CategoryController(IProductRepository<Category> categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        // GET: CategoryController
+        public ActionResult Index()
+        {
+            var categories = categoryRepository.List();
+            return View(categories);
+        }
+
+        // GET: CategoryController/Details/5
+        public ActionResult Details(int id)
+        {
+            var categories = categoryRepository.Find(id);
+            return View(categories);
+        }
+
+        // GET: CategoryController/Create
+        public ActionResult Create()
+        {
+      
+            return View();
+        }
+
+        // POST: CategoryController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category categories)
+        {
+            try
+            {
+                categoryRepository.Add(categories); 
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoryController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var category = categoryRepository.Find(id);
+            return View(category);
+        }
+
+        // POST: CategoryController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Category category)
+        {
+            try
+            {
+                categoryRepository.Update(id, category);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoryController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var category = categoryRepository.Find(id);
+            return View(category);
+        }
+
+        // POST: CategoryController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Category category)
+        {
+            try
+            {
+                categoryRepository.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,212 @@
+﻿using AdminDashboard.Models;
+using AdminDashboard.Models.Repositories;
+using AdminDashboard.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository<Product> productRepository;
+        private readonly IProductRepository<Category> categoryRepository;
+        [Obsolete]
+        private readonly IHostingEnvironment hosting;
+
+        [Obsolete]
+        public ProductController(IProductRepository<Product> productRepository,
+            IProductRepository<Category> categoryRepository, IHostingEnvironment hosting)
+        {
+            this.productRepository = productRepository;
+            this.categoryRepository = categoryRepository;
+            this.hosting = hosting;
+        } 
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            var products = productRepository.List();
+            return View(products);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            var product = productRepository.Find(id);
+            return View(product);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            var model = new ProductCategoryViewModel
+            {
+                Categories = FillSelectList()
+            };
+            return View(model);
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Obsolete]
+        public ActionResult Create(ProductCategoryViewModel model)
+        {
+            try
+            {
+                string fillName = string.Empty;
+                if(model.File != null)
+                {
+                    //save file in upload folder
+                    var upload = Path.Combine(hosting.WebRootPath,"upload");
+                    fillName = model.File.FileName;
+                    string fullPath = Path.Combine(upload, fillName);
+                    model.File.CopyTo(new FileStream(fullPath, FileMode.Create));
+
+                }
+              
+
+                if(model.category_id == -1)
+                {
+                    ViewBag.Message = "Please Select Category!";
+                    // fill list of categories to ignore exception 
+                    var Vmodel = new ProductCategoryViewModel
+                    {
+                        Categories = FillSelectList()
+                    };
+                    return View(Vmodel);
+                }
+                
+                var Category = categoryRepository.Find(model.category_id);
+
+                Product product = new Product
+                {
+                    Id = model.product_id,
+                    product_name = model.product_name,
+                    product_Description = model.product_Description,
+                    product_price = model.product_price,
+                    category = Category,
+                    imageURL = fillName
+
+                
+                };
+                productRepository.Add(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var product = productRepository.Find(id);
+            var model = new ProductCategoryViewModel
+            {
+
+                product_id = product.Id,
+                product_name = product.product_name,
+                product_Description = product.product_Description,
+                product_price = product.product_price,
+                category_id = product.category.category_Id,
+                Categories = categoryRepository.List().ToList(),
+                imageURL = product.imageURL
+           
+
+            };
+         
+            return View(model);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Obsolete]
+        public ActionResult Edit(int id, ProductCategoryViewModel model)
+        {
+            try
+            {
+                string upload = Path.Combine(hosting.WebRootPath, "upload");
+                string fillName = string.Empty;
+                if (model.File != null)
+                {
+                    //save file in upload folder
+                    fillName = model.File.FileName;
+                    string fullPath = Path.Combine(upload, fillName);
+                    string oldURL = model.imageURL;
+                    if(oldURL != fillName) {
+                        // to ckeck that it is not the same image 
+                        model.File.CopyTo(new FileStream(fullPath, FileMode.Create));
+                    }
+                }
+                else
+                {
+                    //in case user doesnot choose imge use the old image 
+                    fillName = model.imageURL;
+                }
+
+                var Category = categoryRepository.Find(model.category_id);
+              
+                Product product = new Product
+                {
+                    Id = model.product_id,
+                    product_name = model.product_name,
+                    product_Description = model.product_Description,
+                    product_price = model.product_price,
+                    category = Category,
+                    imageURL = fillName
+
+                };
+                productRepository.Update(model.product_id, product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+           
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var product = productRepository.Find(id);
+            return View(product);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult confirmDelete(int id)
+        {
+            try
+            {
+                productRepository.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        List<Category> FillSelectList()
+        {
+            var categories = categoryRepository.List().ToList();
+            categories.Insert(0, new Category { category_Id = -1, category_name = " --- Please Select Category ---" });
+            return categories;
+        }
+
+        public ActionResult Search(string term)
+        {
+            var result = productRepository.Search(term);
+            return View("Index", result);
+        }
+    }
+}
